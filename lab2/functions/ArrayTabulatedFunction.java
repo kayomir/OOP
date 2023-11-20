507bf1d21821261bf9db209d6f0d5dcf837f59c8
Kirill <kirilltsikin1@gmail.com>
2023-11-20 18:10:38 +0400

    lab 3

---
+++ lab2/functions/ArrayTabulatedFunction.java
@@ -0,0 +1,198 @@
+package functions;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class ArrayTabulatedFunction extends AbstractTabulatedFunction implements Cloneable{
+
+    protected double [] xValues;
+
+    protected double [] yValues;
+
+    protected int count;
+
+    public ArrayTabulatedFunction(double[] xValues, double[] yValues){
+        this.count = xValues.length;
+        this.xValues = Arrays.copyOf(xValues,xValues.length);
+        this.yValues = Arrays.copyOf(yValues,yValues.length);
+    }
+
+    public ArrayTabulatedFunction(MathFunction source, double xFrom, double xTo, int count) {
+        this.count = count;
+        this.xValues = new double[count];
+        this.yValues = new double[count];
+        if (xFrom < xTo) {
+            this.xValues[0] = xFrom;
+            this.xValues[count-1] = xTo;
+        } else {
+            this.xValues[0] = xTo;
+            this.xValues[count-1] = xFrom;
+        }
+        double delta = (this.xValues[count-1]-this.xValues[0])/(count-1);
+        this.yValues[0] = source.apply(this.xValues[0]);
+        this.yValues[count-1] = source.apply(this.xValues[count-1]);
+        for (int i = 1; i < count; i++){
+            this.xValues[i] = this.xValues[i-1]+delta;
+            this.yValues[i] = source.apply(this.xValues[i]);
+        }
+    }
+
+    @Override
+    protected int floorIndexOfX(double x) {
+        if (x < this.leftBound()) return 0;
+        if (x > this.rightBound()) return count-1;
+
+        int leftBorder = 0;
+        int rightBorder = this.xValues.length - 1;
+        while (leftBorder <= rightBorder) {
+            int middle = (leftBorder + rightBorder) / 2;
+            double current = this.xValues[middle];
+            if (current == x) {
+                return middle;
+            } else if (current < x) {
+                leftBorder = middle + 1;
+            } else {
+                rightBorder = middle - 1;
+            }
+        }
+        return leftBorder-1;
+    }
+
+    @Override
+    protected double extrapolateLeft(double x) {
+        if (count == 1) return yValues[0];
+        return yValues[1]+(((x-xValues[1])/(xValues[0]-xValues[1]))*(yValues[0]-yValues[1]));
+    }
+
+    @Override
+    protected double extrapolateRight(double x) {
+        if (count == 1) return yValues[0];
+        return yValues[count-2]+(((x-xValues[count-2])/(xValues[count-1]-xValues[count-2]))*(yValues[count-1]-yValues[count-2]));
+    }
+
+    @Override
+    protected double interpolate(double x, int floorIndex) {
+        if (count == 1) return yValues[0];
+        return yValues[floorIndex]+((yValues[floorIndex+1]-yValues[floorIndex])/(xValues[floorIndex+1]-xValues[floorIndex]))*(x-xValues[floorIndex]);
+    }
+
+        @Override
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        if (count == 1) return yValues[0];
+        return leftY+((rightY-leftY)/(rightX-leftX))*(x-leftX);
+    }
+
+    @Override
+    public int getCount() {
+        return this.count;
+    }
+
+    @Override
+    public double getX(int index) {
+        return this.xValues[index];
+    }
+
+    @Override
+    public double getY(int index) {
+        return this.yValues[index];
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        this.yValues[index] = value;
+    }
+
+    @Override
+    public int indexOfX(double x) {                 //Поиск индекса значения использует бинарный поиск в массиве
+        if ((x < this.leftBound()) || (x > this.rightBound())) return -1;               //Выход из поиска при искомом значении вне границ значений массива
+        // Создаем границы со значениями индексов крайних значений массива
+        int leftBorder = 0;
+        int rightBorder = this.xValues.length - 1;
+        // пока левая и правая границы поиска не равны
+        while (leftBorder <= rightBorder) {
+            // индекс текущего элемента находится посередине
+            int middle = (leftBorder + rightBorder) / 2;    //Находим центральный элемент массива
+            double current = this.xValues[middle];    //Находим текущий элемент массива
+
+            if (current == x) {
+                // нашли элемент - возвращаем его индекс
+                return middle;
+            } else if (current < x) {
+                // текущий элемент меньше искомого - сдвигаем левую границу
+                leftBorder = middle + 1;
+            } else {
+                // иначе сдвигаем правую границу
+                rightBorder = middle - 1;
+            }
+        }
+        // проверили весь массив, но не нашли элемент
+        return -1;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        if ((y < this.getY(0)) || (y > this.getY(this.count-1))) return -1;
+        int leftBorder = 0;
+        int rightBorder = this.yValues.length - 1;
+        while (leftBorder <= rightBorder) {
+            int middle = (leftBorder + rightBorder) / 2;
+            double current = this.yValues[middle];
+
+            if (current == y) {
+                return middle;
+            } else if (current < y) {
+                leftBorder = middle + 1;
+            } else {
+                rightBorder = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public double leftBound() {
+        return this.xValues[0];
+    }
+
+    @Override
+    public double rightBound() {
+        return this.xValues[this.count-1];
+    }
+
+    @Override
+    public double apply(double x) {
+        if (x < leftBound()) return extrapolateLeft(x);
+        else if (x > rightBound()) return extrapolateRight(x);
+        else if (indexOfX(x) != -1) return getY(indexOfX(x));
+        else return interpolate(x,floorIndexOfX(x));
+    }
+
+    @Override
+    public String toString() {
+        return "{xValues=" + Arrays.toString(xValues) +
+                ", yValues=" + Arrays.toString(yValues) +
+                ", count=" + count +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (!(obj instanceof ArrayTabulatedFunction) || (((ArrayTabulatedFunction) obj).count != this.count)) return false;
+        for (int i = 0 ; i < this.count; ++i) {
+            if (this.xValues[i] != ((ArrayTabulatedFunction) obj).xValues[i]) return false;
+            if (this.yValues[i] != ((ArrayTabulatedFunction) obj).yValues[i]) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xValues,yValues,count);
+    }
+
+    @Override
+    public Object clone() {
+        return new ArrayTabulatedFunction(this.xValues, this.yValues);
+    }
+}
--- lab2/functions/IdentityFunctionTest.java
+++ lab2/functions/IdentityFunctionTest.java
@@ -12,4 +12,28 @@ public class IdentityFunctionTest {
         assertEquals(1, IdentityFunction.apply(1));
         assertNotEquals(1, IdentityFunction.apply(2));
     }
+    @Test
+    public void equals() {
+        MathFunction firstIdentityFunction = new IdentityFunction();
+        MathFunction secondIdentityFunction = new IdentityFunction();
+        MathFunction constantFunction = new ConstantFunction(5.5);
+        assertTrue(firstIdentityFunction.equals(secondIdentityFunction));
+        assertFalse(firstIdentityFunction.equals(constantFunction));
+    }
+
+    @Test
+    public void hashCodeTest() {
+        MathFunction firstIdentityFunction = new IdentityFunction();
+        MathFunction firstIdentityFunctionCopy = firstIdentityFunction;
+        MathFunction secondIdentityFunction = new IdentityFunction();
+        assertEquals(firstIdentityFunction.hashCode(), firstIdentityFunctionCopy.hashCode());
+        assertNotEquals(firstIdentityFunction.hashCode(), secondIdentityFunction.hashCode());
+    }
+
+    @Test
+    public void cloneTest() throws CloneNotSupportedException {
+        MathFunction firstIdentityFunction = new IdentityFunction();
+        Object firstIdentityFunctionClone = ((IdentityFunction) firstIdentityFunction).clone();
+        assertNotEquals(firstIdentityFunction.hashCode(), firstIdentityFunctionClone.hashCode());
+    }
 }
