3d329651daa928138d8eba6a20c92369cf0dad18
Kirill <kirilltsikin1@gmail.com>
2023-12-10 11:54:28 +0400

    lab 5 writeTabulatedFunction

--- lab2/io/FunctionsIO.java
+++ lab2/io/FunctionsIO.java
@@ -7,6 +7,20 @@ public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+        try {
+            PrintWriter printWriter = new PrintWriter(writer);
+            printWriter.println(function.getCount());
+            for (Point point : function) {
+                printWriter.printf("%f %f\n",point.x,point.y);
+            }
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
         DataOutputStream dos = new DataOutputStream(outputStream);
         dos.writeInt(function.getCount());
